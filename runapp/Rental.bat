f9ccaa50135c71327b02ee23408b90c9c32834de
ninew12 <kamonwan670@gmail.com>
2023-12-20 13:49:24 +0700

    update old data

--- runapp/Rental.bat
+++ runapp/Rental.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Starting Node.js server...
-cd /d C:\Users\newyear\Documents\restroom\appserver
-pm2 start server.js --name "server"
+cd /d C:\Users\PC\Application\V2.0\restroom\appserver
+pm2 start --name "server" server.js
 
 
 
