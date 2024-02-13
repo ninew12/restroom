accea577b17807bbb936d0d270264453d7cdfe15
Kamonwan Adeiam <kamoa457@cubes.postbox.in.th>
2024-02-13 10:12:02 +0700

    ีupdate script node run

--- appserver/noderun.js
+++ appserver/noderun.js
@@ -2,9 +2,9 @@ var Service = require('node-windows').Service;
 
 // Create a new service object
 var svc = new Service({
-  name:'Hello World',
+  name:'appserver',
   description: 'The nodejs.org example web server.',
-  script: 'C:\Users\newyear\Documents\restroom\appserver\index.js',
+  script: 'C:\\Users\\newyear\\Documents\\restroom\\appserver\\index.js',
   nodeOptions: [
     '--harmony',
     '--max_old_space_size=4096'
