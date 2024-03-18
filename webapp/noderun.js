90be5f45c2d6c8d6541dc969972bd6a73fa9f806
ninew12 <kamonwan670@gmail.com>
2024-03-18 15:50:22 +0700

    update data

--- webapp/noderun.js
+++ webapp/noderun.js
@@ -2,7 +2,7 @@ var Service = require('node-windows').Service;
 
 // Create a new service object
 var svc = new Service({
-  name:'webapp',
+  name:'webappRestRoom',
   description: 'The nodejs.org example web server.',
   script: 'C:\\Users\\PC\\Application\\V2.0\\restroom\\webapp\\index.js',
   nodeOptions: [
